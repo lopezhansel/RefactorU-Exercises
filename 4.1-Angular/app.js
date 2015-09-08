b94ad509f6af025e78d3ecb9972ff5b9b5ec58b2
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 23:38:43 -0600

    added logic to app.js

--- 4.1-Angular/app.js
+++ 4.1-Angular/app.js
@@ -5,5 +5,22 @@
 */
 var app = angular.module('myapp', []);
 app.controller('MainController', ['$scope', function(){
-	
+	$scope.hideH1  = false;
+	$scope.hideList = true;
+
+	$scope.toggleH1 = function(){         
+		console.log("hello world");
+		if ($scope.hideH1 === false){
+			$scope.hideH1 = true;
+		} else { 
+			$scope.hideH1 = false;
+		}
+	}
+	$scope.toggleList = function(){
+		if ($scope.hideList === true){
+			$scope.hideList = false;
+		} else { 
+			$scope.hideList = true;
+		}
+	}
 }]);
