49c8a3b45abc45b1d40e2770972dfd56f9685f35
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 23:16:16 -0600

    Finished Instructions

--- 4.1-Functions/app.js
+++ 4.1-Functions/app.js
@@ -16,6 +16,12 @@ function negate (int){
 	return 0 - int;
 }
 
+function toArray (a,b,c) {
+	var newArray = [];
+	newArray.push(a,b,c);
+	return newArray;
+}
+
 function startsWithA (text){
 	var input = text;
 	if (input.charAt(0) === "a"){
@@ -23,4 +29,32 @@ function startsWithA (text){
 	}else{
 		return false;
 	}
+}
+
+function excite(input){
+	return input + "!!!";
+}
+
+
+
+function sun (str) {
+	if (str.indexOf('sun') > -1) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+
+function tiny (num){
+	if(num >0 && num < 1) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function getSeconds(str){
+	var splits = str.split(":");
+	return (Number(splits[0])*60) + Number(splits[1]);
 }
