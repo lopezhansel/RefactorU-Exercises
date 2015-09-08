a138aa625f1bdafb49735acc8354f01aa7a3efb7
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 22:45:47 -0600

    Added birthdate Validator

--- 3-SimpleValidator/app.js
+++ 3-SimpleValidator/app.js
@@ -1,9 +1,9 @@
-// has flaw spaces and perios can will pass
+// Phone Number Validator
+//  BUG:  spaces, - negatives numbers and perios can will pass
 function askNumber (){
 	var userNumber = prompt("Write Your Nummber including dashes");
 	alert(validateNumber(userNumber));
 }
-
 function validateNumber (userInput){
 
 	if (userInput.length !== 12){
@@ -18,3 +18,29 @@ function validateNumber (userInput){
 		return'Woo! You Wrote A real Phone Number!';
 	}
 }
+
+// Birthdate Validator
+// BUG: Periods will pass by
+function validBirthDate (input) {
+	var date = input.trim();
+	var month = Number(date.substring(0,2));
+	var day = Number(date.substring(3,5));
+	var year = Number(date.substring(6,8));
+	var error = "Please input a proper Date Format";
+
+	// for (var i = 0; i < date.length ; i++) {	
+	// }
+
+	if (date.length != 8 ) {                                       // checking Format XX/XX/XX.length 
+		return "Not 8 Characters " + error;
+	}else if (date.charAt(2) !== "/" || date.charAt(5) !== "/") {  // checking for slashes
+		return "Need slashes. " + error; 
+	}else if(month >12 || month < 1){                      // checking valid month 1-12
+		return  "not valid Month";
+	}else if (day > 31 || day < 1){                      // checks valid day 1-31
+		return " not valid Day";
+	}else{
+		return "pass";
+	}
+}
+console.log(validBirthDate("1./01/44"));
