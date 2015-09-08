40e352b181d2d350a9d84a4e234673df8eb419ac
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 22:20:39 -0600

    first and final commit

---
+++ 2-StringReporter/app.js
@@ -0,0 +1,9 @@
+var input = prompt('Write anything you want to write')
+
+alert("You entered " + input);
+alert("There are " + input.length + " characters in the word.");
+alert("The third character is '" + input.charAt(2)+ "'");
+alert("Lowercase: " + input.toLowerCase());
+alert("Uppercase: " + input.toUpperCase());
+alert("I have wanted a " + input+" since I was a little boy.");
+alert(input.substring(1,4));
