85ef7bf93990fd6cc00f811ad2d9acacf589f218
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 21:48:36 -0600

    3: This should be a for loop inside  a for loop

--- 1.1- Loops/app.js
+++ 1.1- Loops/app.js
@@ -8,3 +8,17 @@ console.log("2: multiply by twos till 64");
 for (var i = 1; i < 65; i= i*2){
 	console.log(i); 
 }
+//3: This should be a for loop inside  a for loop
+
+console.log("3: divide by 3??");
+for (var i = 0; i < 9; i+=1){
+	if (i<3){
+		console.log(1); 
+	}
+	 else if(i<6){
+		console.log(2);
+	}
+	else{
+		console.log(3);
+	}
+}
