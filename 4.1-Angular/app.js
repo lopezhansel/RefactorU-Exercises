e66671e95e471ff45126a4945df9240dea99f57b
Hansel Lopez <lopezhansel@gmail.com>
2015-09-08 17:54:25 -0600

    Simplified some  logic

--- 4.1-Angular/app.js
+++ 4.1-Angular/app.js
@@ -10,25 +10,14 @@ app.controller('MainController', ['$scope', function($scope){
 	$scope.redText = false;
 
 	$scope.toggleH1 = function(){         
-		console.log("hello world");
-		if ($scope.hideH1 === false){
-			$scope.hideH1 = true;
-		} else { 
-			$scope.hideH1 = false;
-		}
+			$scope.hideH1 = !$scope.hideH1;
 	}
 	$scope.toggleList = function(){
-		if ($scope.hideList === true){
-			$scope.hideList = false;
-		} else { 
-			$scope.hideList = true;
-		}
+			$scope.hideList = !$scope.hideList;
 	}
 	$scope.toggleRedText = function(){
-		if ($scope.redText === true){
-			$scope.redText = false;
-		}else {
-			$scope.redText = true;
-		}
+		$scope.redText = !$scope.redText;
+
 	}
 }]);
+
