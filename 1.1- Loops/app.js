20a5c60d68a83fa6d6c4f0644f1e9410496d5c37
Hansel Lopez <lopezhansel@gmail.com>
2015-09-07 21:46:48 -0600

    2: multiply by two to 64

--- 1.1- Loops/app.js
+++ 1.1- Loops/app.js
@@ -3,3 +3,8 @@ console.log("1: increment to 500 by 100s");
 for (var i = 0; i < 501; i+=100){
 	console.log(i); 
 }
+//2: multiply by two to 64
+console.log("2: multiply by twos till 64");
+for (var i = 1; i < 65; i= i*2){
+	console.log(i); 
+}
