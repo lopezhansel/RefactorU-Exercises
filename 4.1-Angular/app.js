b997b4183e5d6372544356bcdfe183527b3f9d2f
Hansel Lopez <lopezhansel@gmail.com>
2015-09-08 21:56:00 -0600

    Added excite function. Not the best way to add bang.

--- 4.1-Angular/app.js
+++ 4.1-Angular/app.js
@@ -5,9 +5,9 @@
 */
 var app = angular.module('myApp', []);
 app.controller('MainController', ['$scope', function($scope){
+
 	$scope.hideH1  = false;
 	$scope.hideList = true;
-
 
 	$scope.toggleH1 = function(){         
 		$scope.hideH1 = !$scope.hideH1;
@@ -23,5 +23,15 @@ app.controller('MainController', ['$scope', function($scope){
 		$scope.redText3 = (num === 3);
 	}
 
+	$scope.h1 = "Headings 1";
+	$scope.h2 = "Heading 2";
+	$scope.h3 = "Heading 3";
+
+
+	$scope.excite = function (input){
+		if(input ===1){$scope.h1 += "!"};
+		if(input ===2){$scope.h2 += "!"};
+		if(input ===3){$scope.h3 += "!"};
+	}
 }]);
 
