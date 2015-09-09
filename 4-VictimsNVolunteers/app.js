517fe8b4ba91340d9f58b7a525112ffaa6855439
Hansel Lopez <lopezhansel@gmail.com>
2015-09-09 11:58:38 -0600

    intial commit for js4

---
+++ 4-VictimsNVolunteers/app.js
@@ -0,0 +1,9 @@
+var numVictims = prompt ("how many victims do you wish to enter")
+var victimsNames = []
+var victimsStreets = []
+var victimsPhones = []
+
+
+console.log(victimsNames)
+console.log(victimsStreets)
+console.log(victimsPhones)
