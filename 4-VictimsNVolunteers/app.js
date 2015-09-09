cf1f6194bdb511e9d851fc19a38390c0ab472957
Hansel Lopez <lopezhansel@gmail.com>
2015-09-09 12:07:54 -0600

    Prompting user with a for loop

--- 4-VictimsNVolunteers/app.js
+++ 4-VictimsNVolunteers/app.js
@@ -3,6 +3,14 @@ var victimsNames = []
 var victimsStreets = []
 var victimsPhones = []
 
+for (var i = 0; i < numVictims; i++) {
+	var name   = prompt('victim ' + (i+1) + ' name' )
+	var street = prompt('victim ' + (i+1) + ' street' )
+	var phone  = prompt('victim ' + (i+1) + ' phone' )
+	victimsNames.push(name)
+	victimsPhones.push(phone)
+	victimsStreets.push(street)
+};
 
 console.log(victimsNames)
 console.log(victimsStreets)
