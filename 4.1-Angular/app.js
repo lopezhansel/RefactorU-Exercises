95610eeae540785372676b8fd22644ad27333cc5
Hansel Lopez <lopezhansel@gmail.com>
2015-09-09 14:25:10 -0600

    Used the ternary operator in toggleRedText Finished the stopDefAction()

--- 4.1-Angular/app.js
+++ 4.1-Angular/app.js
@@ -13,9 +13,9 @@ app.controller('MainController', ['$scope', '$timeout', function($scope, $timeou
 	}
 
 	$scope.toggleRedText = function(num){
-		$scope.redText1 = (num === 1);
-		$scope.redText2 = (num === 2);
-		$scope.redText3 = (num === 3);
+		$scope.redText1 = (num === 1) ? 'blue' : 'black';
+		$scope.redText2 = (num === 2) ? 'blue' : 'black';
+		$scope.redText3 = (num === 3) ? 'blue' : 'black';
 	}
 
 	$scope.h1 = "Headings 1";
@@ -25,9 +25,12 @@ app.controller('MainController', ['$scope', '$timeout', function($scope, $timeou
 	$scope.excite = function(head){
 		$scope[head] += "!";
 	}
-	$scope.alerto= function(input){
-		console.log(input.target)
+
+	$scope.stopDefAction = function (evt) {
+		var yesOrNo = confirm('are you sure you want to leave');
+    	yesOrNo === true ? 'ok': evt.preventDefault()
 	}
+
 	// this is thed old excite method
 	// $scope.excite = function (input){
 	// 	if(input ===1){$scope.h1 += "!"};
