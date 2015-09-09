87a8315455858c115516cd5459606218829c2e6e
Hansel Lopez <lopezhansel@gmail.com>
2015-09-08 20:00:52 -0600

    Fixed bug: Now only one paragraph turns red at a time

--- 4.1-Angular/app.js
+++ 4.1-Angular/app.js
@@ -7,18 +7,21 @@ var app = angular.module('myApp', []);
 app.controller('MainController', ['$scope', function($scope){
 	$scope.hideH1  = false;
 	$scope.hideList = true;
-	$scope.redText = false;
+
 
 	$scope.toggleH1 = function(){         
 		$scope.hideH1 = !$scope.hideH1;
 	}
 
 	$scope.toggleList = function(){
-		$scope.hideList = !$scope.hideList
+		$scope.hideList = !$scope.hideList;
 	}
-	$scope.toggleRedText = function(){
-		$scope.redText = !$scope.redText;
 
+	$scope.toggleRedText = function(num){
+		$scope.redText1 = (num === 1);
+		$scope.redText2 = (num === 2);
+		$scope.redText3 = (num === 3);
 	}
+
 }]);
 
