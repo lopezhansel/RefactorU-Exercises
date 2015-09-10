3a2db1994deeed608cf0d7cd539749d81d4a2e4e
Hansel <lopezhansel@gmail.com>
2015-09-10 08:18:12 -0600

    added  function to enable edit on the static user info

--- 5-EditInPLace/app.js
+++ 5-EditInPLace/app.js
@@ -1,11 +1,17 @@
 
 var app = angular.module('myApp', [])
 app.controller('MainController', ['$scope', function($scope){
-	$scope.userName = 'User Name';
-	$scope.userEmail = 'me@email.com';
-	$scope.userWork = "Developer"
-	$scope.userSchool = "RefactorU"
-	$scope.userLocation = "Boulder"
+	$scope.userName 	= 	['User Name',false];
+	$scope.userEmail	= 	['me@email.com', false];
+	$scope.userWork	    = 	["Profession",false];
+	$scope.userSchool 	= 	["School",false];
+	$scope.userLocation = 	["Location",false];
 
+	$scope.toggle = function (which) {
+		var not = $scope[which][1]
+		$scope[which][1] =  !not;
+		console.log(not);
+	}
+		
 }])
 
