b176eb352225e0f8fe684ea88661ef1932b07e82
Hansel <lopezhansel@gmail.com>
2015-09-10 14:23:50 -0600

    finished letterCapitalize() function

--- 5.1-jsProblems/app.js
+++ 5.1-jsProblems/app.js
@@ -1,6 +1,13 @@
 function letterCapitalize (str) {
-	var strArray = str.split(' ')
-	console.log(strArray.join(' '))
+	var strArray = str.split(' ');
+
+	for (var i = 0 ; i < strArray.length; i++){
+		var capitalWord = strArray[i].charAt(0).toUpperCase() + strArray[i].slice(1);
+		strArray[i] = capitalWord;
+	}
+
+	return strArray.join(' ');
 }
-letterCapitalize('helo there im here')
+
+console.log(letterCapitalize('hello there im here'))
 
