22678b4440394de4fc9e3e798dac659de268940e
Hansel <lopezhansel@gmail.com>
2015-09-12 17:12:52 -0600

    Finished 4th Function

--- 8-FunctionPractice2/app.js
+++ 8-FunctionPractice2/app.js
@@ -3,12 +3,14 @@ function getName (arg) {
 }
 console.log("Function 1")
 console.log(getName({ name: 'Luisa', age: 25 }))
+console.log('\n')
 function totalLetters (arg) {
 	return arg.join('').length
 }
 console.log("Function 2")
 console.log(totalLetters(['javascript', 'is', 'awesome']))
 console.log(totalLetters(['what', 'happened', 'to', 'my', 'function']))
+console.log('\n')
 
 function keyValue (arg1 , arg2) {
 	// this didnt work I'm not sure why
@@ -21,3 +23,13 @@ function keyValue (arg1 , arg2) {
 }
 console.log("Function 3")
 console.log(keyValue('city', 'Denver'))
+console.log('\n')
+
+function negativeIndex (array, num) {
+	return array[array.length+num]
+}
+console.log('4th Function')
+console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2))
+console.log(negativeIndex(['jerry', 'sarah', 'sally'], -1) )
+console.log('\n')
+
