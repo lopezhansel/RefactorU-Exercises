d7ba7c915cbc0d9e73779de51904c08d22be7061
Hansel <lopezhansel@gmail.com>
2015-09-12 22:05:19 -0600

    added NgRouter

---
+++ 0-myStore/js/controllers/ProductsController.js
@@ -0,0 +1,41 @@
+app.controller('ProductsController', ['$scope', function($scope){
+	$scope.products = [
+		{
+			name: 'product 1',
+			price: 13.54,
+			rating: 5,
+			img: 'http://placehold.it/300x300',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
+		},
+		{
+			name: 'product 1',
+			price: 13.54,
+			rating: 5,
+			img: 'http://placehold.it/300x300',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
+		},
+		{
+			name: 'product 1',
+			price: 13.54,
+			rating: 5,
+			img: 'http://placehold.it/300x300',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
+		},
+		{
+			name: 'product 1',
+			price: 13.54,
+			rating: 5,
+			img: 'http://placehold.it/300x300',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
+		},
+		{
+			name: 'product 1',
+			price: 13.54,
+			rating: 5,
+			img: 'http://placehold.it/300x300',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
+		}
+
+	]
+	
+}])
