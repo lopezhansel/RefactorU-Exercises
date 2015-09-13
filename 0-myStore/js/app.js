8dce254a593a617134849c16796d687cb1556b04
Hansel <lopezhansel@gmail.com>
2015-09-13 00:05:17 -0600

    Added working NgRouter and NgStorage Will used these to save user reviews in local storage

--- 0-myStore/js/app.js
+++ 0-myStore/js/app.js
@@ -1,13 +1,16 @@
-var app = angular.module('storeApp',['ngRoute']);
+var app = angular.module('storeApp',['ngRoute','ngStorage']);
 app.config(function($routeProvider){
-	$routeProvider
-    .when('/store', { 
+  $routeProvider
+    .when('/', { 
       controller: 'ProductsController', 
       templateUrl: 'views/storeFront.html' 
     })
+     .when('/product/:id', { 
+      controller: 'ProductsController', 
+      templateUrl: 'views/productPage.html' 
+    })
+
     .otherwise({ 
-      redirectTo: '/store' 
-    });   	
+      redirectTo: '/' 
+    });     
 });
-
-
--- 0-myStore/js/controllers/ProductsController.js
+++ 0-myStore/js/controllers/ProductsController.js
@@ -1,35 +1,35 @@
-app.controller('ProductsController', ['$scope', function($scope){
+app.controller('ProductsController', ['$scope','$routeParams','$localStorage', function($scope,$routeParams,$localStorage){
 	$scope.products = [
 		{
-			name: 'product 1',
+			name: 'Awesome 1',
 			price: 13.54,
 			rating: 5,
 			img: 'http://placehold.it/300x300',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
 		},
 		{
-			name: 'product 1',
+			name: 'Here 2',
 			price: 13.54,
 			rating: 5,
 			img: 'http://placehold.it/300x300',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
 		},
 		{
-			name: 'product 1',
+			name: 'There 3',
 			price: 13.54,
 			rating: 5,
 			img: 'http://placehold.it/300x300',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
 		},
 		{
-			name: 'product 1',
+			name: 'My 4',
 			price: 13.54,
 			rating: 5,
 			img: 'http://placehold.it/300x300',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac neque ac quam cursus auctor. Maecenas eget hendrerit dui. Integer ornare tellus non lobortis imperdiet. '
 		},
 		{
-			name: 'product 1',
+			name: 'What 2',
 			price: 13.54,
 			rating: 5,
 			img: 'http://placehold.it/300x300',
@@ -37,5 +37,9 @@ app.controller('ProductsController', ['$scope', function($scope){
 		}
 
 	]
-	
+	$scope.select = $routeParams.id
+	$scope.$storage = $localStorage.$default({ x: 42});
+	$scope.addToX = function  () {
+		$scope.$storage.x++;
+	}
 }])
