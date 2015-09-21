f29d3742d41d55b975413a4b2795c648cf152225
Hansel <lopezhansel@gmail.com>
2015-09-21 08:24:33 -0600

    caesarCipher() is almost done. It only works with lowerCase for now

--- 15-problemSet3/main.js
+++ 15-problemSet3/main.js
@@ -33,13 +33,39 @@ function dashInsert (num) {
 }
 console.log(dashInsert(1234556));
 
+var lowerCase = []
+	upperCase =  []
+for (var i = 97; i < 123; i++) {
+	lowerCase.push(String.fromCharCode(i))
+};
+for (var i = 65; i < 91; i++) {
+	upperCase.push(String.fromCharCode(i))
+};
 
-function caesarCipher (str,num) {
+
+// function caesarCipherSingle (str,num) { // breaks at y and z and also any other sign character
+// 	var output = []
+// 	for (var i = 0; i < str.length; i++) {
+// 		var charCode = str.charCodeAt(i);
+// 		output.push(String.fromCharCode(charCode + num) )
+// 	};
+// 	return output.join('')
+// }
+function caesarCipherSingle (str,num) {
 	var output = []
 	for (var i = 0; i < str.length; i++) {
-		var charCode = str.charCodeAt(i);
-		output.push(String.fromCharCode(charCode + num) )
+		var charCode = lowerCase.indexOf(str[i]);
+		output.push(lowerCase[charCode + num] )
 	};
 	return output.join('')
 }
+
+function caesarCipher (str,num) {
+	var arr = str.split(' ')
+	var output = arr.map(function  (element) {
+		return caesarCipherSingle(element, num)
+	})
+	return output.join(' ')
+}
+console.log(caesarCipher('caesar cipher' , 2));
 console.log(caesarCipher('Caesar Cipher' , 2));
