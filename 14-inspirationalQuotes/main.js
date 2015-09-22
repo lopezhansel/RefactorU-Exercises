d1cc60cec154350b80849143e7f990ad0ff8dc02
Hansel <lopezhansel@gmail.com>
2015-09-21 21:53:15 -0600

    added a few movie Quotes

--- 14-inspirationalQuotes/main.js
+++ 14-inspirationalQuotes/main.js
@@ -1,5 +1,31 @@
 var app = angular.module('quotes', ['ngStorage'])
 app.controller('quotesCtrl', ['$scope','$localStorage', function ($scope,$localStorage) {
-	$scope.quote1 = 'Inspirational Quotes'
+	$scope.quote1 = 'Movie Quotes'
 	$scope.$storage = $localStorage.$default({here:['hello']})
+	$scope.quotes = [{
+		author: 'Forest Gump',
+		quote : "My momma always said, 'Life was like a box of chocolates. You never know what you're gonna get.'",
+		rating: 5
+	},{
+		author: 'Johnny Castle' ,
+		quote: "Nobody puts Baby in the corner",
+		rating: 3
+	},{
+		author: "Travis Bickle" ,
+		quote: "You talkin' to me?" ,
+		rating: 3
+	},{
+		author: "Tony Montana",
+		quote:  "Say 'hello' to my little friend!",
+		rating: 4
+	},{
+		author: "Det. Robert Thorn",
+		quote: "Soylent Green is people!" ,
+		rating: 2
+	},{
+		author: "The Terminator",
+		quote: "Hasta la vista, baby." ,
+		rating: 5
+	}]
+
 }])
