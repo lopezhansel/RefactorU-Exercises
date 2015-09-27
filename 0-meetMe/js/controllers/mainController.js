50c69906b9fc2a7b054e5d3f4f2580f131e8ab30
Hansel <lopezhansel@gmail.com>
2015-09-27 15:48:03 -0600

    Made distanceFrom() to trigger location

--- 0-meetMe/js/controllers/mainController.js
+++ 0-meetMe/js/controllers/mainController.js
@@ -1,59 +1,56 @@
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  // convert degres to radian
-  if (typeof(Number.prototype.toRad) === "undefined") {
-  Number.prototype.toRad = function() {
-    return this * Math.PI / 180;
-  }
-}
-// Central Subtended Angle Method
-// Great Circle Method
-// toronto
- var lat2 = 43.677
- var lon2 = -79.630
-// johanasBurg
-  var lat1 = -26.133;
-  var lon1 = 28.242;
-	// window.onload = function() {
-	//   $scope.startPos;
-
-	//   navigator.geolocation.getCurrentPosition(function(position) {
-	//  $scope.startPos = position;
-
- //    var lat1 = $scope.startPos.coords.latitude;
-	//   var lon1 = $scope.startPos.coords.longitude;
- //    $scope.lat = lat1
- //    $scope.longg = lon1
-	//   });
-	// };
-
-  var R = 6371 / 1.609344; //Earth Median radius in Kilometres / convert to km to miles
-  var φ1 = lat1.toRad();
-  var φ2 = lat2.toRad();
-  var Δφ = (lat2-lat1).toRad();
-  var Δλ = (lon2-lon1).toRad();
-
-  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
-
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-  var distance = (R * c) ;
-  console.log(distance);
-
-
+//--------------------------------------------------------Material desing content--------------------------------------------------------------
   $scope.layout = 'row'
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
   $scope.imagePath = 'https://material.angularjs.org/latest/img/washedout.png';
- $scope.names=['Igor Minar', 'Brad Green', 'Dave Geddes', 'Naomi Black', 'Greg Weber', 'Dean Sofer', 'Wes Alvaro', 'John Scott', 'Daniel Nadasi'];
+  $scope.names=['Igor Minar', 'Brad Green', 'Dave Geddes', 'Naomi Black', 'Greg Weber', 'Dean Sofer', 'Wes Alvaro', 'John Scott', 'Daniel Nadasi'];
+//------------------------------------------------Location----------------------------------------------------------------------
+  if (typeof(Number.prototype.toRad) === "undefined") { // convert degres to radian
+    Number.prototype.toRad = function() {
+      return this * Math.PI / 180;
+    }
+  }
+  var lat2 = 25.6667   // monterrey , mexico 
+  var lon2 = -100.3000 // monterrey = boulder actual distaance = 1030 miles
 
-	// navigator.geolocation.watchPosition(function(position) {
-	//   document.getElementById('currentLat').innerHTML = position.coords.latitude;
-	//   document.getElementById('currentLon').innerHTML = position.coords.longitude;
-	// }); 
+  window.onload = function() { // get my location and set lat1 lon1 && $scope.lat $scope.longg
+    var startPos;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      startPos = position;
+      $scope.lat = startPos.coords.latitude;
+      $scope.longg = startPos.coords.longitude;
+      lat1 = $scope.lat 
+      lon1 = $scope.longg 
+      console.log('My Coodinates are: \n Lat: ', lat1,"\n Lon: ",  lon1);
+      distanceFrom()
+    });   
+  };
+  // navigator.geolocation.watchPosition(function(position) {
+  //   $scope.lat = position.coords.latitude;
+  //   $scope.longg = position.coords.longitude;
+  // }); 
+  function distanceFrom (arg1 , arg2) { // Central Subtended Angle Method || Great Circle Method
+    var R = 6371 / 1.609344; //Earth Median radius in Kilometres / convert to km to miles
+    var φ1 = lat1.toRad();
+    var φ2 = lat2.toRad();
+    var Δφ = (lat2-lat1).toRad();
+    var Δλ = (lon2-lon1).toRad();
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + // arc length
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var distance = (R * c) ;
+    console.log("I am " , Math.round(distance), "miles from Monterrey, Mexico");
+  }
+
+
 }]);
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
 
 app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
   $scope.items = items;
