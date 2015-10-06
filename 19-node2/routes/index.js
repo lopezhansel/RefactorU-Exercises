e3708f72a72be7f3bc42c427d2bd1d1f25971953
Hansel <lopezhansel@gmail.com>
2015-10-05 23:00:05 -0600

    First renders

--- 19-node2/routes/index.js
+++ 19-node2/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hansels site', nums: [1,2,3,4,5] });
 });
 
 module.exports = router;
