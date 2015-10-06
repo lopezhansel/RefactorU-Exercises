98b6e8760d3649a1316184d31677b6a79d83dc56
Hansel <lopezhansel@gmail.com>
2015-10-05 18:47:12 -0600

    Finished colors script

--- 18-node1/colors.js
+++ 18-node1/colors.js
@@ -4,7 +4,10 @@ request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd419861
   if (!error && response.statusCode == 200) {
      var colors = JSON.parse(body);
   }
-
+  var filtered = colors.filter(function(element){
+  	return (process.argv[2].toLowerCase() === element.name.toLowerCase())
+  })
+  console.log(filtered[0].rgb.r,filtered[0].rgb.g,filtered[0].rgb.b);
 
 })
 
