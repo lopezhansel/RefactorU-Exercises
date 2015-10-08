5157df92a8dcb179e0a2eb70bfb61bfc46f98aa5
Hansel <lopezhansel@gmail.com>
2015-10-07 23:01:22 -0600

    Fixed error

--- 0-final/index.js
+++ 0-final/index.js
@@ -3,8 +3,16 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/',function(req,res){
-	app.send('hello');
+	res.send('hello');
 });
 
 var port = 3000;
