db8f8215832c34f605a3ab6d175a8532d11cd83a
Hansel <lopezhansel@gmail.com>
2015-10-08 23:03:20 -0600

    added post router handler for video submisions

--- 24-node6-contestApp/index.js
+++ 24-node6-contestApp/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+// console.log(bodyParser);
 var app = express();
 
 app.use(bodyParser.json());
@@ -9,6 +9,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile('index.html',{root:'./public/views'});
+});
+
+app.post('/postVideos',function(req,res){
+	res.send(req.body);
 });
 
 var port = 3000;
--- 24-node6-contestApp/public/js/main.js
+++ 24-node6-contestApp/public/js/main.js
@@ -1,5 +1,12 @@
 var app = angular.module('contestApp', []);
 app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
 	$scope.hello = "hello world";
-	
+	$scope.videoSubmit = function  () {
+		$http.post('/postVideos', $scope.videoSubmited).then(function(returnData){
+			console.log(returnData.data);
+			$scope.returnObj = returnData.data;
+		});
+	};
 }]);
+
+
