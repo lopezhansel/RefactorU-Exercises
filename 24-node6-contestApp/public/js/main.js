3b5e94a0ac29b3127fce01420cce94612ed1f85d
Hansel <lopezhansel@gmail.com>
2015-10-09 19:22:08 -0600

    Requesting video submissions from server and posting new ones

--- 24-node6-contestApp/public/js/main.js
+++ 24-node6-contestApp/public/js/main.js
@@ -1,12 +1,20 @@
 var app = angular.module('contestApp', []);
 app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
+
 	$scope.hello = "hello world";
+
+	$http.get('/submissionsApi').then(function  (serverResponse) {
+		$scope.returnObj = serverResponse.data;
+	});
+
 	$scope.videoSubmit = function  () {
 		$http.post('/postVideos', $scope.videoSubmited).then(function(returnData){
-			console.log(returnData.data);
 			$scope.returnObj = returnData.data;
 		});
 	};
+
 }]);
 
 
+
+   
