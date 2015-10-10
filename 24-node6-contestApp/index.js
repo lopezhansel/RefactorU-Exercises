e6194cf03c849291a523a177e03eba3012d67878
Hansel <lopezhansel@gmail.com>
2015-10-09 19:21:23 -0600

    added different routes in express

--- 24-node6-contestApp/index.js
+++ 24-node6-contestApp/index.js
@@ -7,16 +7,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+
+var allSubmissions = [];
+
 app.get('/', function(req, res){
   res.sendFile('index.html',{root:'./public/views'});
 });
 
+app.get('/submissionsApi',function  (req,res) {
+	res.send(allSubmissions);
+});
+
+app.get('/BestVideos',function  (req,res) {
+	res.sendFile('bestvideos.html',{root:"./public/views"});
+});
 app.post('/postVideos',function(req,res){
-	res.send(req.body);
+	allSubmissions.push(req.body);
+	console.log(req.body);
+
+	res.send(allSubmissions	);
 });
 
 var port = 3000;
 app.listen(port, function(){
   console.log('Server running on port ' + port);
+  // console.log(	'hello');
 
 });
