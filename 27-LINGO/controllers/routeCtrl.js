fe4ac31a11378f3823049d02692fb3b9fe845981
Hansel <lopezhansel@gmail.com>
2015-10-17 16:13:31 -0600

    adding a typoCheck function not ready

--- 27-LINGO/controllers/routeCtrl.js
+++ 27-LINGO/controllers/routeCtrl.js
@@ -1,46 +1,51 @@
 var Data = require('../models/dataDb');
 var api = require('myApis/api');
-var gTranslate  = require('google-translate')(api);
+var gTranslate = require('google-translate')(api);
 
 module.exports = {
-	readData     : readData,
-	createData   : createData,
-	home         : home,
-	translating  : translating
+	readData: readData,
+	createData: createData,
+	home: home,
+	translating: translating
 
 };
 
-function readData (req,res) {
-	Data.find({},function  (err,docs) {
+function readData(req, res) {
+	Data.find({}, function(err, docs) {
 		res.send(docs);
 	});
 }
 
-function createData (req,res) {
+function createData(req, res) {
 	var insertData = new Data(req.body);
 	console.log(insertData);
 	insertData.save();
-	res.send( insertData);
+	res.send(insertData);
 }
 
-function home (req,res) {
+function home(req, res) {
 	// console.log(req);
 	var path = req._parsedOriginalUrl.path;
-	res.sendFile('/views' +path+ ".html",{root:"./public"});
+	res.sendFile('/views' + path + ".html", {
+		root: "./public"
+	});
 }
 
 
-function translating (req,res) {
+function translating(req, res) {
 	console.log(req.body);
 
-	gTranslate.translate(req.body.sourceText,req.body.newLang,function  (err, translation) {
+	gTranslate.translate(req.body.sourceText, req.body.newLang, function(err, translation) {
 		console.log(translation.translatedText);
-		if (err){res.send(err); }
-		else{res.send(translation.translatedText); }
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(translation.translatedText);
+		}
 	});
 
 }
-var wordsAray = ["one","two","three","four","five","six","seven","eight" ,"nine","ten"];
+var wordsAray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 // var count = 0;
 
 
@@ -48,14 +53,23 @@ var wordsAray = ["one","two","three","four","five","six","seven","eight" ,"nine"
 //   console.log(translation.translatedText);
 // });
 
-
-function testLang (srcLang,destLang,guess,quizWord) {
-	var anser = null;
-	gTranslate.translate(quizWord,destLang,function  (err, translation) {
-		if (guess.toLowerCase() === translation.translatedText.toLowerCase()){anser =  true;}
-		else{anser =  false; }
-		console.log(anser);
-	});
-	// return anser;
+function typoCheck(guess, right) {
+	if (guess.length === right.length) {
+		
+	}
 }
-var test = testLang('en','es','hola','hello');
+
+function testLang(srcLang, destLang, guess, quizWord) { // this is to see if the guess matches exactly to translation
+	var rightOrWrong = null;
+	gTranslate.translate(quizWord, destLang, function(err, translation) {
+		if (guess.toLowerCase() === translation.translatedText.toLowerCase()) {
+			rightOrWrong = "right";
+			return console.log(rightOrWrong);
+		} else {
+			console.log(typoCheck(guess, translation.translatedText));
+		}
+	});
+	// return rightOrWrong;
+}
+testLang('en', 'es', 'hala', 'hello');
+
