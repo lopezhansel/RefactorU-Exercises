7551c4e90139c94fc2796a3f056de81c90e74f48
Hansel <lopezhansel@gmail.com>
2015-10-18 15:40:41 -0600

    Finishing validation functions

--- 27-LINGO/scrap.js
+++ 27-LINGO/scrap.js
@@ -1,52 +1,3 @@
-var Data = require('../models/dataDb');
-var api = require('myApis/api');
-var gTranslate = require('google-translate')(api);
-
-module.exports = {
-	readData: readData,
-	createData: createData,
-	home: home,
-	translating: translating
-
-};
-
-function readData(req, res) {
-	Data.find({}, function(err, docs) {
-		res.send(docs);
-	});
-}
-
-function createData(req, res) {
-	var insertData = new Data(req.body);
-	console.log(insertData);
-	insertData.save();
-	res.send(insertData);
-}
-
-function home(req, res) {
-	// console.log(req);
-	var path = req._parsedOriginalUrl.path;
-	res.sendFile('/views' + path + ".html", {
-		root: "./public"
-	});
-}
-
-function translating(req, res) {
-	console.log(req.body);
-
-	gTranslate.translate(req.body.sourceText, req.body.newLang, function(err, translation) {
-		console.log(translation.translatedText);
-		if (err) {
-			res.send(err);
-		} else {
-			res.send(translation.translatedText);
-		}
-	});
-
-}
-// gTranslate.translate('My name is Hansel', 'es', function(err, translation) {
-//   console.log(translation.translatedText);
-// });
 
 var wordsAray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 // var count = 0;
@@ -107,7 +58,7 @@ function missingOneLetter (guessAnswer,correctAnswer) {
 	return outputArray.join('');
 }
 
-console.log(missingOneLetter('javacript','javascript'));
+console.log(missingOneLetter('vascript','javascript'));
 
 // function testLang(srcLang, destLang, guess, quizWord) { // this is to see if the guess matches exactly to translation
 // 	var rightOrWrong = null;
