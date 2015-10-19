ee123c461de52fa4664bc5458bd1a809e6f3657a
Hansel <lopezhansel@gmail.com>
2015-10-18 20:48:20 -0600

    Minor changes to angular script

--- 27-LINGO/public/js/ngApp.js
+++ 27-LINGO/public/js/ngApp.js
@@ -3,14 +3,15 @@ var app = angular.module('Lingo', []);
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.greeting = 'Welcome To Lingo App';
 	$scope.wordTranslate = {};
-	$scope.fiveLanguages = ['es','fr','gr','it',''];
-
+	$scope.fiveLanguages = ['es','fr','gr','it','ru'];
+	$scope.test = {};
+	
 	$scope.getData = function() {
-		$http.get('/api/readData').then(function(response) {
-			console.log(response.data);
+		$http.get('/api/getWords').then(function(response) {
+			$scope.test.quiz = response.data;
 		});
 	};
-	// $scope.getData();
+	$scope.getData();
 
 	$scope.postData = function() {
 		$http.post('/translating', $scope.wordTranslate) 
@@ -19,13 +20,18 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 				console.log($scope.wordTranslate.sourceText);
 				if($scope.wordTranslate.sourceText == response.data){
 					console.log("you have err");
-					$scope.translatedText = "you have an error"
+					$scope.translatedText = "you have an error";
 				}
 				else{$scope.translatedText = response.data;}
 			});
 		// $scope.getData();
 	};
-
+	$scope.validateAnswer = function  () {
+		$http.post('/validateAnswer',$scope.test).then(function  (response) {
+			$scope.realAnswer = response.data;
+		});
+	};
 
 
 }]);
+// console.log();
