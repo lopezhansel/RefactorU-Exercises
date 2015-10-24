fd7cf9f1870530cc81b90e04808a15d6c429389e
Hansel <lopezhansel@gmail.com>
2015-10-23 23:39:25 -0600

    Getting passport set up

--- 29-ChatApp/server.js
+++ 29-ChatApp/server.js
@@ -28,4 +28,102 @@ var userSchema = mongoose.Schema({
 });
 var User = mongoose.model('user', userSchema);
 
-app.server = app.listen(3000);
+
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            bcrypt.compare(password, user.password, function(error, response){
+                if (response === true){
+                    return done(null,user);
+                }
+                else {
+                    return done(null, false);
+                }
+            });
+        });
+    }
+));
+
+
+app.isAuthenticated = function(req, res, next){
+ if(req.isAuthenticated()){return next(); }
+    res.redirect('/');
+};
+
+
+app.isAuthenticatedAjax = function(req, res, next){
+    if(req.isAuthenticated()){return next(); }
+    res.send({error:'not logged in'});
+};
+
+
+
+app.get('/', function(req, res){
+    res.sendFile('/html/login.html', {root: './public'});
+});
+
+
+app.post('/signup', function(req, res){
+    bcrypt.genSalt(10, function(error, salt){
+        bcrypt.hash(req.body.password, salt, function(hashError, hash){
+            var newUser = new User({
+                username: req.body.username,
+                password: hash,
+            });
+            newUser.save(function(saveErr, user){
+                if ( saveErr ) { res.send({ err:saveErr }); }
+                else { 
+                    req.login(user, function(loginErr){
+                        if ( loginErr ) { res.send({ err:loginErr }); }
+                        else { res.send({success: 'success'}); }
+                    });
+                }
+            });
+            
+        });
+    });
+});
+
+app.post('/login', function(req, res, next){
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.send({error : 'something went wrong :('}); }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.send({success:'success'});
+        });
+    })(req, res, next);
+});
+
+
+
+
+
+
+
+console.log(Date.now());
+
+app.server = app.listen(4000);
