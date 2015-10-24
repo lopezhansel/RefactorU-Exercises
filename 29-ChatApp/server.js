1fc446a79ecc08944e23507a3663becdbe3caa6c
Hansel <lopezhansel@gmail.com>
2015-10-23 23:13:33 -0600

    Connecting mongoose

--- 29-ChatApp/server.js
+++ 29-ChatApp/server.js
@@ -1,12 +1,31 @@
-
 var express = require('express');
 var app = express();
+// module.exports = app;
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-    res.sendFile('/html/chat.html', {root: './public'});
-});
-app.server = app.listen(3000);
 
-var io = require("socket.io");
-var socketServer = io(app.server);
+var session = require('express-session');
+
+app.sessionMiddleware = session({
+  secret: 'c0mPl3xP4ssw0rD',
+  resave: false,
+  saveUninitialized: true,
+});
+app.use(app.sessionMiddleware);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/chat');
+
+var userSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+var User = mongoose.model('user', userSchema);
+
+app.server = app.listen(3000);
