2b0205a29b8442eb072d8355a721978ddb3055bb
Hansel <lopezhansel@gmail.com>
2015-10-24 23:11:43 -0600

    Adding Chat View and Chat controller

---
+++ 29-ChatApp/public/js/chat.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', []);
+app.controller('mainController', ['$scope', '$http', function($scope, $http){
+
+        $http({
+            method : 'GET',
+            url    : '/api/me',
+        }).then(function(returnData){
+            if ( returnData.data.user ) {
+                $scope.user = returnData.data.user;
+            }
+        });
+
+     
+    }]);
