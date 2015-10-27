6de78ff965f81a0c0716f1e5e01273c720544699
Hansel <lopezhansel@gmail.com>
2015-10-26 23:54:17 -0600

    Trying to Enable Chat

--- 29-ChatApp/public/js/chat.js
+++ 29-ChatApp/public/js/chat.js
@@ -1,21 +1,27 @@
 var app = angular.module('app', []);
-app.controller('mainController', ['$scope', '$http', function($scope, $http){
+app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+    $scope.chatMessage = '';
+    $scope.loggedInUsers = [];
+    $scope.messageHistory = [];
 
-        $http({
-            method : 'GET',
-            url    : '/api/me',
-        }).then(function(returnData){
-            if ( returnData.data.user ) {
-                $scope.user = returnData.data.user;
-            }
-        });
-        var socket = io();
+    $http({
+        method: 'GET',
+        url: '/api/me',
+    }).then(function(returnData) {
+        if (returnData.data.user) {
+            $scope.user = returnData.data.user;
+        }
+    });
+    var socket = io();
 
-        socket.on('loggedInUsers', function  (data) {
-            console.log('loggedInUsers' , data);
-            $scope.loggedInUsers = data;
-            $scope.$apply();
+    socket.on('loggedInUsers', function(data) {
+        console.log('loggedInUsers', data);
+        $scope.loggedInUsers = data;
+        $scope.$apply();
 
-        });
-        
-    }]);
+    });
+    $scope.sendMessage = function(event) {
+        socket.emit('chatMessage', $scope.chatMessage);
+
+    };
+}]);
--- 29-ChatApp/server.js
+++ 29-ChatApp/server.js
@@ -1,19 +1,19 @@
-var express    = require('express');
-var session    = require('express-session');
+var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
-var app        = express();
+var app = express();
 app.use(express.static(__dirname + '/public'));
 app.sessionMiddleware = session({
-  secret: 'c0mPl3xP4ssw0rD',
-  resave: false,
-  saveUninitialized: true,
+    secret: 'c0mPl3xP4ssw0rD',
+    resave: false,
+    saveUninitialized: true,
 });
 app.use(app.sessionMiddleware);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 
-	extended: true 
+app.use(bodyParser.urlencoded({
+    extended: true
 }));
 
 
@@ -23,13 +23,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/chat');
 
 var userSchema = mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
 });
 var User = mongoose.model('user', userSchema);
 // == MODELS END == //
 
- // == PASSPORT START == //
+// == PASSPORT START == //
 
 var bcrypt = require('bcryptjs');
 var passport = require('passport');
@@ -52,16 +59,21 @@ passport.deserializeUser(function(id, done) {
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
+        User.findOne({
+            username: username
+        }, function(err, user) {
+            if (err) {
+                return done(err);
             }
-            bcrypt.compare(password, user.password, function(error, response){
-                if (response === true){
-                    return done(null,user);
-                }
-                else {
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect username.'
+                });
+            }
+            bcrypt.compare(password, user.password, function(error, response) {
+                if (response === true) {
+                    return done(null, user);
+                } else {
                     return done(null, false);
                 }
             });
@@ -71,66 +83,98 @@ passport.use(new LocalStrategy(
 
 
 
-app.isAuthenticated = function(req, res, next){
-	console.log(req.isAuthenticated());
- if(req.isAuthenticated()){return next(); }
+app.isAuthenticated = function(req, res, next) {
+    console.log(req.isAuthenticated());
+    if (req.isAuthenticated()) {
+        return next();
+    }
     res.redirect('/');
 };
 
 
-app.isAuthenticatedAjax = function(req, res, next){
-    if(req.isAuthenticated()){return next(); }
-    res.send({error:'not logged in'});
+app.isAuthenticatedAjax = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.send({
+        error: 'not logged in'
+    });
 };
 
 // == PASSPORT END == //
 
 
-app.get('/', function(req, res){
-    res.sendFile('/html/login.html', {root: './public'});
+app.get('/', function(req, res) {
+    res.sendFile('/html/login.html', {
+        root: './public'
+    });
 });
 
 
-app.post('/signup', function(req, res){
-    bcrypt.genSalt(10, function(error, salt){
-        bcrypt.hash(req.body.password, salt, function(hashError, hash){
+app.post('/signup', function(req, res) {
+    bcrypt.genSalt(10, function(error, salt) {
+        bcrypt.hash(req.body.password, salt, function(hashError, hash) {
             var newUser = new User({
                 username: req.body.username,
                 password: hash,
             });
-            newUser.save(function(saveErr, user){
-                if ( saveErr ) { res.send({ err:saveErr }); }
-                else { 
-                    req.login(user, function(loginErr){
-                        if ( loginErr ) { res.send({ err:loginErr }); }
-                        else { res.send({success: 'success'}); }
+            newUser.save(function(saveErr, user) {
+                if (saveErr) {
+                    res.send({
+                        err: saveErr
+                    });
+                } else {
+                    req.login(user, function(loginErr) {
+                        if (loginErr) {
+                            res.send({
+                                err: loginErr
+                            });
+                        } else {
+                            res.send({
+                                success: 'success'
+                            });
+                        }
                     });
                 }
             });
-            
+
         });
     });
 });
 
-app.post('/login', function(req, res, next){
+app.post('/login', function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.send({error : 'something went wrong :('}); }
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.send({
+                error: 'something went wrong :('
+            });
+        }
         req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.send({success:'success'});
+            if (err) {
+                return next(err);
+            }
+            return res.send({
+                success: 'success'
+            });
         });
     })(req, res, next);
 });
 
 
 
-app.get('/chat', app.isAuthenticated, function(req, res){
-    res.sendFile('/html/chat.html', {root: './public'});
+app.get('/chat', app.isAuthenticated, function(req, res) {
+    res.sendFile('/html/chat.html', {
+        root: './public'
+    });
 });
 
-app.get('/api/me', app.isAuthenticatedAjax, function(req, res){
-    res.send({user:req.user});
+app.get('/api/me', app.isAuthenticatedAjax, function(req, res) {
+    res.send({
+        user: req.user
+    });
 });
 
 
@@ -142,27 +186,29 @@ var loggedInUsers = {};
 
 
 var socketServer = io(app.server);
-socketServer.use(function(socket, next){
+socketServer.use(function(socket, next) {
     app.sessionMiddleware(socket.request, {}, next);
 });
 
-socketServer.on("connection", function(socket){
-	if ( socket.request.session && socket.request.session.passport && socket.request.session.passport.user ) {
-		
+socketServer.on("connection", function(socket) {
+    if (socket.request.session && socket.request.session.passport && socket.request.session.passport.user) {
         var id = socket.request.session.passport.user;
-        User.findById(id, function(error, user){
+        User.findById(id, function(error, user) {
 
 
             loggedInUsers[user.username] = true;
 
             socketServer.emit('loggedInUsers', loggedInUsers);
+            socket.on('chatMessage', function(data) {
+                socketServer.emit('chatMessage', {sender: user.username, content: data });
+
+            });
 
 
 
         });
-	}
+    }
 
-	
 
 
 });
