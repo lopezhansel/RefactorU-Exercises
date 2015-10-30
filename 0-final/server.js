0d8078e61cb72c91c55b806521cf79276a31fb77
Hansel <lopezhansel@gmail.com>
2015-10-30 00:34:55 -0600

    Randomly changing User Lat and Lon  simulating user Movement

--- 0-final/server.js
+++ 0-final/server.js
@@ -22,12 +22,12 @@ require("./config/myPassport");
 
 
 
-
 var port = 80;
 app.server = app.listen(port, function() {
     console.log('Final Started', Date.now());
 });
 
+// #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00
 
 var io = require("socket.io");
 var socketServer = io(app.server);
@@ -37,41 +37,53 @@ socketServer.use(function(socket, next) {
     app.sessionMiddleware(socket.request, {}, next);
 });
 
-// #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00  #FFFF00
 var sendUser = [{
-    __v: 0,
-    pictureSm: 'https://randomuser.me/api/portraits/thumb/women/52.jpg',
-    pictureMd: 'https://randomuser.me/api/portraits/med/women/52.jpg',
-    pictureLg: 'https://randomuser.me/api/portraits/women/52.jpg',
-    lon: -105,
-    lat: 40.015,
-    cell: '0766-563-698',
-    phone: '017687 22811',
-    email: 'sophia.armstrong@example.com',
-    lastName: 'armstrong',
-    firstName: 'sophia',
-    dob: 2281986,
-    gender: 'female',
-    password: '$2a$10$9qCjH7dc0oF1dznsYt5lzuYXGGcGxqpcdmdIXPfyglPcG.PsPDmZu',
-    username: 'goldengoose450',
-}];//sendUser
+    __v        : 0,
+    pictureSm  : 'https://randomuser.me/api/portraits/thumb/women/52.jpg',
+    pictureMd  : 'https://randomuser.me/api/portraits/med/women/52.jpg',
+    pictureLg  : 'https://randomuser.me/api/portraits/women/52.jpg',
+    lon        : -105,
+    lat        : 40.015,
+    cell       : '0766-563-698',
+    phone      : '017687 22811',
+    email      : 'sophia.armstrong@example.com',
+    lastName   : 'armstrong',
+    firstName  : 'sophia',
+    dob        : 2281986,
+    gender     : 'female',
+    password   : '$2a$10$9qCjH7dc0oF1dznsYt5lzuYXGGcGxqpcdmdIXPfyglPcG.PsPDmZu',
+    username   : 'goldengoose450',
+}]; //sendUser
 
-// lat: 40.0150,
-// lat: 40.0005,
+//  one mile is about  .0145 degrees
+
 
 socketServer.on("connection", function(socket) {
-    User.find({}).limit(4).exec(function(err, users) {
-        socketServer.emit('loggedInUsers', users);
-        // setInterval(function() {
+            User.find({}).limit(100).exec(function(err, users) {
+                socketServer.emit('loggedInUsers', users);
+                setInterval(function() {
+                    users.forEach(function (user) {
+                        // randomNum = Math.floor((Math.random() * 2) + 1);
+                        // ((randomNum ===1 ) ? user.lat  += 0.00241666 : user.lon  += 0.00241666)
+                        // var num = 0.00241666;
+                        var num = 0.00011666;
 
-        //     // console.log(sendUser[0].lat  += 0.00241666);
-        //     console.log(sendUser[0].lat  -= 0.00001);
-        //     socketServer.emit('loggedInUsers', sendUser);
-        // }, 100);
-        // console.log(users);
-        // socketServer.emit('loggedInUsers', {});
+                        var addOrSub = Math.floor((Math.random() * 2) + 1);
+                        var lonOrLat = Math.floor((Math.random() * 2) + 1);
+                        var distance = ((addOrSub === 1)? num : -num);
+                        ((lonOrLat === 1 ) ? user.lat  += distance : user.lon  += distance);
 
-    });// User.find({})
+                        // user.lat  += 0.00241666;
+                        // user.lon  += 0.00241666;
+                    });
+                    // console.log(users[0].lat  += 0.00241666);
+                    // console.log(users[0].lat  -= 0.00001);
+                    socketServer.emit('loggedInUsers', users);
+                }, 5);
+                console.log(users);
+                socketServer.emit('loggedInUsers', {});
+
+            }); // User.find({})
 
 
     if (socket.request.session && socket.request.session.passport && socket.request.session.passport.user) {
@@ -105,9 +117,8 @@ socketServer.on("connection", function(socket) {
                 loggedInUsers[user.username] = false;
                 socketServer.emit('loggedInUsers', loggedInUsers);
 
-            });//socket.on('disconnect'
+            }); //socket.on('disconnect'
         });
     } // if(loggedIn == true)
 
-});//socketServer.on("connection",
-
+}); //socketServer.on("connection",
