e8c08c6c674385e30c4b169152af1d63b99cd32c
Hansel <lopezhansel@gmail.com>
2015-11-03 21:25:31 -0700

    Adding Request Form Trigger

--- Velocetti/public/js/controllers/indexController.js
+++ Velocetti/public/js/controllers/indexController.js
@@ -1,5 +1,13 @@
 app.controller('indexController', ['$scope', '$mdSidenav', 'userService', '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", 'leafletData', "$location","$timeout", 
 	function($scope, $mdSidenav, userService, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, leafletData, $location,$timeout) {
+	$scope.topDirections = ['left', 'up'];
+	 $scope.bottomDirections = ['down', 'right'];
+	 $scope.isOpen = false;
+	 $scope.availableModes = ['md-fling', 'md-scale'];
+	 $scope.selectedMode = 'md-fling';
+	 $scope.availableDirections = ['up', 'down', 'left', 'right'];
+	 $scope.selectedDirection = 'up';
+
 
 	$scope.redirect = function(urlStr) {
 		$location.path(urlStr);
@@ -14,9 +22,7 @@ app.controller('indexController', ['$scope', '$mdSidenav', 'userService', '$rout
 	};
 
 	$scope.me = {};
-
 	$scope.me = userService.me;
-
 	$scope.selectedIndex = 2;
 	$scope.switchTabs = function(leftOrRight) {
 		console.log("switchTabs");
@@ -25,22 +31,56 @@ app.controller('indexController', ['$scope', '$mdSidenav', 'userService', '$rout
 		if ($scope.selectedIndex < 0) {
 			$scope.selectedIndex = 0;
 		}
-
 	};
 
 	$scope.toggleSidenav = function(menuId) {
 		$mdSidenav(menuId).toggle();
+	};
+	$scope.newRequest = function(ev, index) {
+	  $scope.userPassed = index;
+	  $mdDialog.show({
+	      locals: {
+	        currentUserPopUP: $scope.userPassed
+	      },
+	      controller: requestController,
+	      templateUrl: '/views/requestView.html',
+	      parent: angular.element(document.body),
+	      targetEvent: ev,
+	      clickOutsideToClose: true
+	    })
+	    .then(function(answer) {
+	      $scope.status = 'You said the information was "' + answer + '".';
+	    }, function() {
+	      $scope.status = 'You cancelled the dialog.';
+	    });
 	};
 
 
 }]);
 
 
-// setMapCenter()
-// $mdMedia()
 
-
-// // $scope.me
-// selectedIndex
-// smallmouse892
-// tunafish
+function requestController($scope, $mdDialog, $http) {
+  $scope.hide = function() {$mdDialog.hide(); };
+  $scope.cancel = function() {$mdDialog.cancel(); };
+  $scope.answer = function(answer) {$mdDialog.hide(answer); };
+  $scope.signup = function  () {
+    $mdDialog.hide();
+  };
+  $scope.logion = function(argument) {
+    console.log($scope.signupForm);
+    $http({
+      method: 'POST',
+      url: '/login',
+      data: $scope.signupForm
+    }).then(function(returnData) {
+      console.log(returnData);
+      if (returnData.data) {
+        window.location.href = "/";
+      } else {
+        console.log(returnData);
+      }
+    });
+    $mdDialog.hide();
+  };
+}
